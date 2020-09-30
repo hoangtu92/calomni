36c4468d5a1500d17c51a20ddfd5fd476c1c7fc0
ML <mlcodesign1@gmail.com>
2020-09-30 11:09:46 +0700

    update

--- database/migrations/2020_07_09_031915_create_jobs_table.php
+++ database/migrations/2020_07_09_031915_create_jobs_table.php
@@ -36,7 +36,7 @@ class CreateJobsTable extends Migration
 
             $table->longText("log")->nullable(true)->comment("job log");
 
-            $table->enum("status", [Job::PENDING, Job::FAILED, Job::RUNNING, Job::COMPLETED])->default(Job::PENDING);
+            $table->enum("status", [Job::FAILED, Job::RUNNING, Job::COMPLETED])->default(Job::RUNNING);
 
             $table->timestamps();
         });
